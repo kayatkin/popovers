d8f0b4a044b9d350803b18463b0a1860131e357e
kayatkin <120170506+kayatkin@users.noreply.github.com>
2024-03-10 23:00:29 +0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader,'style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: './index.html',
+    }),
+
+    new MiniCssExtractPlugin(),
+
+    new CopyPlugin({
+      patterns: [
+        { from: './src/css/styles.css', to: 'styles.css' },
+      ],
+    }),
+  ],
+  devServer: {
+    port: 9000,
+    static: path.join(__dirname, 'dist'),
+  },  
+};
